57549c5b27e0672525fc1685cf3a1324ff185914
pedro-design <84478860+pedro-design@users.noreply.github.com>
2022-12-08 17:13:15 -0600

    Update retina_layer.py
    This works fast and uses feature wise retina

--- retina_layer.py
+++ retina_layer.py
@@ -1,48 +1,45 @@
 #use in cnn or 4D tensors
 class retina_module(nn.Module):
-    def __init__(self,inp,features,size=8,device="cpu",prob=0.7):
+    def __init__(self,inp,features,inp_sz,size=8,lstm_features=32 ,device="cpu",prob=0.85):
         super(retina_module,self).__init__()
         
         self.filter_maxprob=prob
-        
-        self.input_conv1= nn.Conv2d(inp,features,3,padding="same")
-        self.input_conv2= nn.Conv2d(features,features,3,padding="same")
-        self.out_conv= nn.Conv2d(features,features,3,padding="same")
         self.size =size
+        self.stride= int((size-1)/2)
         self.masks = features
-        
-        self.q1_predictor= nn.Conv2d(features,1,3,padding="same")
-        self.v1_predictor= nn.Conv2d(features,1,3,padding="same")
+        self.lstm_F = lstm_features
+        self.depth_conv = nn.Conv2d(inp, features, kernel_size=3, groups=inp,padding="same")
         self.act1 = nn.LeakyReLU()
-        self.act2 =nn.Sigmoid()
-        self.adv_pool = nn.AvgPool2d((2,2))
-        self.device=device
+        self.act2 =nn.LeakyReLU()
+        self.lstm_0 =  nn.LSTMCell(inp_sz, lstm_features, bias=True)
+        self.lstm_1 =  nn.LSTMCell(lstm_features, 2, bias=True)
 
     def forward(self,x):
         #you can add more layers
-        x=self.input_conv1(x)
-        x=self.act1 (x)
-        x=self.input_conv2(x)
-        x=self.act1 (x)
-       
-        #PASS SQUEEZE TENSOR
-        batch_size = x.size(0)
-        x =  x.view(-1, x.size(1),self.size ,self.size )  
-       
-        #now create new batch 
-        #att_1
-        at=  self.q1_predictor(x)
-        att= self.v1_predictor(x)
-        x1 = self.act2 (at*att).squeeze() 
-        
-        zeros = torch.zeros((batch_size, self.masks,self.size,self.size),device=self.device)
-        mask = torch.where(x1>x1.max()*self.filter_maxprob)[0]
-        if mask.size(0)>batch_size:
-            sz =100
-        else:
-            sz = mask.size(0)
-        zeros[:sz] = x[mask[sz]]
-        x = zeros
-        x=self.out_conv(x)
-        #(batch, chanels/features, x, y)
+        B, C, H, W = x.shape
+        start = []
+        padded = F.pad(x, (self.size // 2, self.size // 2, self.size // 2, self.size // 2))
+        index = 0
+        for image in x.split(1,dim=0):
+            state_H1 = torch.zeros((image.size(2), self.lstm_F),device=device)
+            state_C1 = torch.zeros((image.size(2), self.lstm_F),device=device)
+            
+            state_H2 = torch.zeros((image.size(2), 2),device=device)
+            state_C2 = torch.zeros((image.size(2) ,2),device=device)
+            features_retina = []
+            step_IND = 0
+            for step in image.split(1,dim=1):
+                step = step.squeeze()
+                state_H1,state_C1= self.lstm_0(step,(state_H1,state_C1 ))
+                state_H2,state_C2= self.lstm_1(state_H1,(state_H2,state_C2 ))
+                out_ =state_H2.argmax(axis=0)
+                features_retina.append(padded[index,step_IND, out_[1] : out_[1]+self.size, out_[ 0] : out_[ 0]+self.size].unsqueeze(0).unsqueeze(0))
+                step_IND = step_IND+1
+            out=  torch.cat(features_retina,dim=0)
+            index = index+1
+         
+            start.append(out)
+
+        x = torch.cat(start,axis=0)
+        x=  self.depth_conv (x)
         return x
