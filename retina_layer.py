2ff54b05570ec5ebd1a65849817e47f77ef8c794
pedro-design <84478860+pedro-design@users.noreply.github.com>
2022-12-08 08:47:52 -0600

    Update retina_layer.py
    Reworked some things

--- retina_layer.py
+++ retina_layer.py
@@ -1,22 +1,21 @@
 #use in cnn or 4D tensors
 class retina_module(nn.Module):
-    def __init__(self,inp,features,size= 8,device="cpu"):
+    def __init__(self,inp,features,size=8,device="cpu",prob=0.7):
         super(retina_module,self).__init__()
         
+        self.filter_maxprob=prob
         
         self.input_conv1= nn.Conv2d(inp,features,3,padding="same")
         self.input_conv2= nn.Conv2d(features,features,3,padding="same")
         self.out_conv= nn.Conv2d(features,features,3,padding="same")
-        self.size =(int((size)/2))
+        self.size =size
         self.masks = features
-        self.x_predictor= nn.Conv2d(features,features,3,padding="same")
-        self.y_predictor= nn.Conv2d(features,features,3,padding="same")
-        self.hiden_prob_conv = nn.Conv2d(1,features,self.size,padding="same")
         
-        #optional
-        #self.hiden_conv =  nn.Conv2d(features,features,2,padding="same")
+        self.q1_predictor= nn.Conv2d(features,1,3,padding="same")
+        self.v1_predictor= nn.Conv2d(features,1,3,padding="same")
         self.act1 = nn.LeakyReLU()
-        self.act2 =nn.LeakyReLU()
+        self.act2 =nn.Sigmoid()
+        self.adv_pool = nn.AvgPool2d((2,2))
         self.device=device
 
     def forward(self,x):
@@ -26,16 +25,24 @@ class retina_module(nn.Module):
         x=self.input_conv2(x)
         x=self.act1 (x)
        
-        x= self.act2(x)
-        x_p=self.x_predictor(x)
-        y_p=self.y_predictor(x)
-        #compute retina mask
-        mask = (x_p*y_p)+x
-        mask = self.act1(mask)
-        tam  = self.size*2 #retina size
-        reshape_mask = mask.reshape(mask.size(0),-1,tam,tam).max(axis=1).values.unsqueeze(0)    
-        reshape_mask =reshape_mask.permute(1,0,2,3)
-        x = self.hiden_prob_conv(reshape_mask)
+        #PASS SQUEEZE TENSOR
+        batch_size = x.size(0)
+        x =  x.view(-1, x.size(1),self.size ,self.size )  
+       
+        #now create new batch 
+        #att_1
+        at=  self.q1_predictor(x)
+        att= self.v1_predictor(x)
+        x1 = self.act2 (at*att).squeeze() 
+        
+        zeros = torch.zeros((batch_size, self.masks,self.size,self.size),device=self.device)
+        mask = torch.where(x1>x1.max()*self.filter_maxprob)[0]
+        if mask.size(0)>batch_size:
+            sz =100
+        else:
+            sz = mask.size(0)
+        zeros[:sz] = x[mask[sz]]
+        x = zeros
         x=self.out_conv(x)
         #(batch, chanels/features, x, y)
         return x
