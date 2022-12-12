fe0b5865cf2e1f9f0efab266a6218e2de666f241
pedro-design <84478860+pedro-design@users.noreply.github.com>
2022-12-11 20:29:18 -0600

    Update retina_layer.py
    is slow but works well

--- retina_layer.py
+++ retina_layer.py
@@ -1,6 +1,6 @@
 #use in cnn or 4D tensors
 class retina_module(nn.Module):
-    def __init__(self,inp,features,inp_sz,size=8,lstm_features=64 ,device="cpu"):
+    def __init__(self,inp,features,inp_sz,size=8,lstm_features=32 ,device="cpu"):
         super(retina_module,self).__init__()
         
         
@@ -12,47 +12,50 @@ class retina_module(nn.Module):
         self.depth_conv = nn.Conv2d(inp, features, kernel_size=3, groups=inp,padding="same")
         self.act1 = nn.LeakyReLU()
         self.act2 =nn.LeakyReLU()
-        
-        
-        self.lstm_0 =  nn.LSTMCell(inp_sz, lstm_features, bias=True)
-        self.lstm_1 =  nn.LSTMCell(lstm_features,lstm_features, bias=True)
-        self.position_predictor = nn.Linear(lstm_features,2)
+        self.lstm_0 =  nn.Transformer(d_model=inp_sz, nhead=4, num_encoder_layers=1, num_decoder_layers=1, dim_feedforward=lstm_features)
+        self.position_predictor = nn.Linear(inp_sz,2)
         self.set = 0
         self.device=device
 
     def forward(self,x):
 
         B, C, H, W = x.shape
-        
-      
-        
+
         start = []
-        
-        padded = F.pad(x, (self.size // 2, self.size // 2, self.size // 2, self.size // 2))
+        padded = nn.functional.pad(x, (self.size // 2, self.size // 2, self.size // 2, self.size // 2))
         index = 0
         for image in x.split(1,dim=0):
-            state_H1 = torch.zeros((image.size(2), self.lstm_F),device=device)
-            state_C1 = torch.zeros((image.size(2), self.lstm_F),device=device)
-            
-            state_H2 = torch.zeros((image.size(2),  self.lstm_F),device=device)
-            state_C2 = torch.zeros((image.size(2) , self.lstm_F),device=device)
+            features_retina = []
+            step_IND = 0
+            image = image[0]
+            state_C2 =  self.lstm_0(image,image )
+            state_C2 = self.position_predictor(state_C2)
+            for coords in state_C2.split(1,dim=0):
+                coords = coords[0].argmax(axis=0)
+                a=padded[index,step_IND, coords[1] : coords[1]+self.size, coords[ 0] : coords[ 0]+self.size]
+                
+                features_retina.append(padded[index,step_IND, coords[0] : coords[0]+self.size, coords[ 1] : coords[ 1]+self.size].unsqueeze(0).unsqueeze(0))
+                step_IND = step_IND+1
+            out=  torch.cat(features_retina,dim=1)
+            index = index+1
+            start.append(out)
+        x = torch.cat(start,axis=0)
+        x=  self.depth_conv (x)
+        return x
             features_retina = []
             step_IND = 0
             for step in image.split(1,dim=1):
                 step = step.squeeze()
-
                 state_H1,state_C1= self.lstm_0(step,(state_H1,state_C1 ))
                 state_H2,state_C2= self.lstm_1(state_H1,(state_H2,state_C2 ))
                 state_C2 = self.position_predictor(state_H2)
                 out_ =state_C2.argmax(axis=0)
-          #      print(out_.shape)
                 features_retina.append(padded[index,step_IND, out_[1] : out_[1]+self.size, out_[ 0] : out_[ 0]+self.size].unsqueeze(0).unsqueeze(0))
                 step_IND = step_IND+1
             out=  torch.cat(features_retina,dim=0)
             index = index+1
             start.append(out)
-            #print(out)
-         #   print(self.state_C.shape)
+
         x = torch.cat(start,axis=0)
         x=  self.depth_conv (x)
         return x
